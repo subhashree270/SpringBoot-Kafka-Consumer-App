e135f180a9473981cff69f74e82b573168bf4cb2
Subhashree <pandasubhashree270@gmail.com>
2024-09-05 22:48:43 +0530

    first commit

---
+++ src/main/java/in/subha/Application.java
@@ -0,0 +1,23 @@
+package in.subha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import in.subha.constants.AppConstants;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+
+	@KafkaListener(topics = AppConstants.TOPIC, groupId="group_ashokit_order")
+	public void subscribeMsg(String order) {
+		System.out.print("*** Msg Recieved From Kafka *** :: ");
+		System.out.println(order);
+	}
+
+}
